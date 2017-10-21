b1d66f7341649b782251329c80498a0716b50af2
DestelloVeloz <40174677@live.napier.ac.uk>
2017-10-21 19:26:42 +0100

    using template inheritance

--- sourcecode/app.py
+++ sourcecode/app.py
@@ -1,9 +1,12 @@
+
+
+import ConfigParser
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/status/<currstatus>')
 
@@ -14,5 +17,21 @@ def status(currstatus):
 def page_not_found(error):
     return "Opps, the URL you requested for does not exist",404
 
+def init(app):
+    config = ConfigParser.ConfigParser()
+    try:
+config_location = "etc/defaults.cfg"
+        config.read(config_location)
+        app.config['DEBUG'] = config.get("config", "debug")
+        app.config['ip_address'] = config.get("config", "ip_address
+")
+        app.config['port'] = config.get("config", "port")
+        app.config['url'] = config.get("config", "url")
+    except:
+        print "Could not read configs from: ", config_location
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=9116, debug=True)
+    init(app)
+    app.run(host=app.config['ip_address'], port=int(app.config['port']), debug=True)
+    
+
