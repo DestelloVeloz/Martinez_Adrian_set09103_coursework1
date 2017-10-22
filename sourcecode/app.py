8cb298528a826650ab0156f37b8c76358d309017
DestelloVeloz <40174677@live.napier.ac.uk>
2017-10-22 12:31:49 +0100

    slight name change to log file

--- sourcecode/app.py
+++ sourcecode/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    this_route=url_for('.home')
+    this_route=url_for('home')
     return render_template('home.html')
 
 @app.route('/login/')
