ac6c12f4323c11f2ff2ac53c3c5a400ca0b889f7
DestelloVeloz <40174677@live.napier.ac.uk>
2017-10-22 11:51:57 +0100

    animation on home page completed

--- sourcecode/app.py
+++ sourcecode/app.py
@@ -1,9 +1,12 @@
 import ConfigParser
-from flask import Flask, render_template
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    this_route=url_for('.root')
     return render_template('home.html')
 
 @app.route('/login/')
@@ -30,9 +33,18 @@ def init(app):
         app.config['url'] = config.get("config", "url")
     except:
         print "Could not read configs from: ", config_location
-
+def logs(app):
+    log_pathname = app.config['log_location'] + app.config['log_file']
+    file_handler = RotatingFileHandler(log_pathname, maxBytes=1024* 1024 * 10 , backupCount =1024)
+    file_handler.setLevel( app.config['log_level'] )
+    formatter = logging.Formatter("%(levelname)s | %(asctime)s |\
+        %(module)s | %(funcName)s | %(message)s")
+    file_handler.setFormatter(formatter)
+    app.logger.setLevel( app.config['log_level'] )
+    app.logger.addHandler(file_handler)
 if __name__ == "__main__":
     init(app)
+    logs(app)
     app.run(host=app.config['ip_address'], port=int(app.config['port']), debug=app.config['debug'])
     
 
